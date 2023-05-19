9cc12edfdfe6e99de3a9488f312144a41d846a72
Johnny Yeng <a2902793@gmail.com>
2023-05-19 22:18:17 +0800

    feat: fix outdated docker node version

---
+++ prerequisite.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo yum update && sudo yum install git -y
+
+# Install Node Using NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+~/.nvm/nvm.sh
+nvm install v18.11.0
+nvm use v18.11.0
+
+# Install Docker
+sudo yum install docker
+sudo usermod -a -G docker ec2-user
+id ec2-user
+newgrp docker
