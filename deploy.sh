a92bba7e886fa53acb27283f5f7a2b204799b6e9
Johnny Yeng <a2902793@gmail.com>
2023-05-22 15:41:32 +0800

    feat:  revised deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
-sudo yum update && sudo yum install git -y
+# Prerequisites before running this script
+## sudo yum update && sudo yum install git -y
+## git clone https://github.com/aws-educate-tw/aws-serverless-ai-stories.git
+## cd aws-serverless-ai-stories/
+## git checkout dev
+## chmod +x deploy.sh
+## aws configure
+## ./deploy.sh
 
 # Install Node Using NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-~/.nvm/nvm.sh
+chmod +x ~/.nvm/nvm.sh
+echo 'export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"' >> cd ~/.bashrc
+source ~/.bashrc
 nvm install v18.11.0
 nvm use v18.11.0
+npm install aws-cdk -g
 
 # Install Docker
-sudo yum install docker
+sudo yum install docker -y
 sudo usermod -a -G docker ec2-user
 id ec2-user
-newgrp docker
+newgrp docker <<EONG
+EONG # newgrp starts a new subshell, 'EONG' is required in order to prevent script from stopping 
+sudo systemctl enable docker
+sudo systemctl start docker
 
+# Bootstrap
+cdk bootstrap "aws://$(aws sts get-caller-identity --query "Account" --output text)/us-east-1"
 # Deploy
 npm run install:all
 npm run deploy
